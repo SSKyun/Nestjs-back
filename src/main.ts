67d1c9265822c2cdf45500117795e3d6a6054538
SSKyun <tbvjzoshszld@gmail.com>
2023-03-31 20:27:07 +0900

    mqtt modify

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,27 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as config from 'config';
 import * as cookieParser from 'cookie-parser'
-import { MicroserviceOptions } from '@nestjs/microservices';
 import { Transport } from '@nestjs/microservices/enums';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as mqtt from 'mqtt';
 
+const mqttOptions = {
+  url: '210.223.152.36:22',
+  options: {
+    clientId: 'nestjs-microservice',
+    username: 'root',
+    password: 'amol@dkagh',
+    rejectUnauthorized: false,
+  },
+};
+
+const mqttClient = mqtt.connect(mqttOptions.url, mqttOptions.options);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
   const serverConfig = config.get('server');
   const port = serverConfig.port;
   app.use(cookieParser());
-  
   app.enableCors({
   origin : ["http://localhost:3000"],
   credentials: true
@@ -22,10 +31,4 @@ async function bootstrap() {
   await app.listen(port);
   Logger.log(`Application running on port ${port}`)
 }
-bootstrap();
-
-
-// const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule,{
-//   transport : Transport.MQTT,
-  
-// });
+bootstrap(); 
